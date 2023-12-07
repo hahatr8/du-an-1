40cd8ae17cf7175e54104d851d5024d7d051e725
diepdx <diepdxph32317@fpt.edu.vn>
2023-12-07 21:02:17 +0700

    add to develop

--- src/Models/Cart.php
+++ src/Models/Cart.php
@@ -13,7 +13,7 @@ class Cart extends Model
         'soluong_sp',
     ];
 
-
+    // public function getTotalPrice($items)
     public function getItems($id)
     {
         $sql = "SELECT * FROM giohang WHERE id_user = {$id} ORDER BY id DESC";
@@ -137,12 +137,5 @@ class Cart extends Model
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(':id_user', $idUser);
         $stmt->execute();
-
-
     }
-
-
-
-
-
 }
